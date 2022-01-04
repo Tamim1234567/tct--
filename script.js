7737419deed668eedb097894fc742e3ccc4b38f5
Tamim <86140595+Tamim1234567@users.noreply.github.com>
2022-01-03 21:19:35 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function dn(){
+    document.querySelector(".links").style.display="none";
+    document.querySelector(".hide").style.display="none";
+    document.querySelector(".show").style.display="block";
+
+}
+function dn2(){
+    document.querySelector(".links").style.display="block";
+    document.querySelector(".hide").style.display="block";
+    document.querySelector(".show").style.display="none";
+}
